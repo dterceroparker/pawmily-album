b25a62beacb1b8ec8c36308e486428d675a28bd3
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-19 19:06:57 -0400

    routes import and mount complete

---
+++ controllers/posts.js
@@ -0,0 +1,6 @@
+import { Post } from "../models/post.js";
+
+
+export {
+
+}
---
+++ routes/posts.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+
+const router = Router()
+
+// router.get('/', ) 
+
+export {
+  router
+}
