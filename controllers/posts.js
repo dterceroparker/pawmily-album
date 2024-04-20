5c8424142ef2a5f902e176aca8e7c4aacc7231fe
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-19 20:47:36 -0400

    create posts/new.ejs/index.ejs and views/posts/new.css

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,11 @@
 import { Post } from "../models/post.js";
 
+function newPost(req, res) {
+  res.render('posts/new', {
+    title: 'Add Post'
+  })
+}
 
 export {
-
+  newPost as new,
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { isLoggedIn } from '../middleware/middleware.js'
+import * as postsCtrl from '../controllers/posts.js'
 
 const router = Router()
 
-// router.get('/', ) 
+//GET localhost:3000/posts/new
+router.get('/new', postsCtrl.new) 
 
 export {
   router
